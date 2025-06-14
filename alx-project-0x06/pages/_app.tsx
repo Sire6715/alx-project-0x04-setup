116338ea92173afa7143bea8dcd7c052bd4eb54a
Prince <uchepibe@gmail.com>
2025-06-14 03:09:36 +0100

    Redux counter app

---
+++ alx-project-0x06/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { Provider } from "react-redux";
+import store from "@/store/store";
+import "@/styles/globals.css";
+import Layout from "@/components/layouts/Layout";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
+}
